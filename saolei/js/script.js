17cf0cc87e9b3ded821b1e3b3e109922350ba3d1
zzsealy <1147575371@qq.com>
2020-07-21 00:27:30 +0800

    标记功能

--- saolei/js/script.js
+++ saolei/js/script.js
@@ -16,6 +16,18 @@ new Vue ({
                 e.target.className = "li"
             }
         },
+        markBox(box, e) {
+            console.log("e", e)
+            this.closeMenu(box) // 关掉菜单
+            var flagDom = document.getElementById('flag'+box) // 获取到每次点击对应的dom
+            if (flagDom.style.display == 'none') {
+                flagDom.style.display = 'block'
+                e.target.innerHTML = "撤标" // 改变了li标签的值
+            } else {
+                flagDom.style.display = 'none'
+                e.target.innerHTML = "标记"
+            }
+        },
         openMenu(boxNum) {  // 打开盒子菜单
             var menuDom = document.getElementById('menu' + boxNum)
             menuDom.style.display = "block";
@@ -25,7 +37,7 @@ new Vue ({
                 }
             }
         },
-        closeMenu(boxNum){
+        closeMenu(boxNum){  // 关闭盒子菜单
             var menuDom = document.getElementById('menu' + boxNum)
             menuDom.style.display = "none";
         }
