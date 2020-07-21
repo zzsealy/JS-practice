22f8d42ab4cd9ec31767a8ba67391458f268baaf
zzsealy <1147575371@qq.com>
2020-07-21 10:10:36 +0800

    完成埋雷操作

--- saolei/js/script.js
+++ saolei/js/script.js
@@ -1,12 +1,31 @@
 new Vue ({
     el: "#game-ui",
     data: {
-        boxs: 100,
-        inputNum: ""
+        boxs: 0,
+        inputNum: "10",
+        boomNum:20,
+        booms:[] // 把20个雷选出来放进去
     },
     methods: {
+        // 生成随机数
+        randomNum() {
+            var num = Math.floor(Math.random() * (this.boxs-1) + 1)
+            return num
+        },
         settingGame() {
             this.boxs = parseInt(this.inputNum) * parseInt(this.inputNum)
+            for (let i = 0; i < this.boomNum; i++) {
+                this.addBoom()
+            }
+            console.log(this.booms)
+        },
+        addBoom() {
+            var num = this.randomNum()
+            if (this.booms.indexOf(num) == -1 ) {
+                this.booms.push(num)
+            } else {
+                this.addBoom()
+            }
         },
         listMove(e) {
             // console.log(e.target.className)
@@ -17,7 +36,6 @@ new Vue ({
             }
         },
         markBox(box, e) {
-            console.log("e", e)
             this.closeMenu(box) // 关掉菜单
             var flagDom = document.getElementById('flag'+box) // 获取到每次点击对应的dom
             if (flagDom.style.display == 'none') {
