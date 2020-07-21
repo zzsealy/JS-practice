639e58de15e956e52e1a4123efb603c14dc3faf3
zzsealy <1147575371@qq.com>
2020-07-21 11:54:14 +0800

    判断盒子周围的雷数目

--- saolei/js/script.js
+++ saolei/js/script.js
@@ -7,7 +7,82 @@ new Vue ({
         booms:[] // 把20个雷选出来放进去
     },
     methods: {
-        // 生成随机数
+        // 点击扫雷
+        standBox(box) {
+            this.closeMenu(box) // 隐藏菜单。
+            var boomBoxDom = document.getElementById("boomBox"+box)
+            boomBoxDom.className = "boom-box activ"
+        },
+        boomLoop(box) {
+            console.log(box)
+            var has_boom = 0  // 点击的一个盒子周围八个相邻的方向炸弹的总数。
+            // 左边
+            var box_left = box - 1
+            if (this.lastNum(box_left) >= 1) {
+                if (this.booms.indexOf(box_left)!= -1) {
+                    has_boom += 1;
+                }
+            }
+            // 右边
+            var box_right = box + 1
+            if (this.lastNum(box_right) != 1) { // != 1说明 有右边
+                if (this.booms.indexOf(box_right)!= -1) {
+                    has_boom += 1;
+                }
+            }
+            // 上边
+            var box_top = box - 10
+            if (box_top > 0) {
+                if (this.booms.indexOf(box_top)!= -1) {
+                    has_boom += 1;
+                }
+                //上左
+                var box_top_left = box_top -1
+                if (this.lastNum(box_top_left) >= 1) {
+                    if (this.booms.indexOf(box_top_left)!= -1) {
+                        has_boom += 1;
+                    }
+                }
+                // 上右
+                var box_top_right = box_top + 1
+                if (this.lastNum(box_top_right) != 1) {
+                    if (this.booms.indexOf(box_top_right)!= -1) {
+                        has_boom += 1;
+                    }
+                } 
+            }
+            // 下边
+            var box_bottom = box + 10
+            if (box_bottom < this.boxs ) {
+                if (this.booms.indexOf(box_bottom)!= -1) {
+                    has_boom += 1;
+                }
+                //下左
+                var box_bottom_left = box_bottom -1
+                if (this.lastNum(box_bottom_left) >= 1) {
+                    if (this.booms.indexOf(box_bottom_left)!= -1) {
+                        has_boom += 1;
+                    }
+                }
+                // 下右
+                var box_bottom_right = box_bottom + 1
+                if (this.lastNum(box_bottom_right) != 1) {
+                    if (this.booms.indexOf(box_bottom_right)!= -1) {
+                        has_boom += 1;
+                    }
+                } 
+            }
+
+            var aroundDom = document.getElementById('around'+box)
+            aroundDom.innerHTML = has_boom;
+        },
+        // 判断个位, 判断最左还是最右
+        lastNum(box) {
+            var boxstr = box.toString().split('')
+            var last = boxstr[boxstr.length-1]
+            return parseInt(last)
+        },
+        // 生成一个随机数
         randomNum() {
             var num = Math.floor(Math.random() * (this.boxs-1) + 1)
             return num
@@ -54,10 +129,20 @@ new Vue ({
                     this.closeMenu(i)
                 }
             }
+            for (let i = 0; i < this.boxs; i++) {
+                const element = this.boxs[i];
+                if(this.booms.indexOf(i + 1) == -1) {  // 判断是不是雷区，不是雷区为true。
+                    // 计算周围有多少雷。
+                    this.boomLoop(i + 1)
+                }
+            }
         },
         closeMenu(boxNum){  // 关闭盒子菜单
             var menuDom = document.getElementById('menu' + boxNum)
             menuDom.style.display = "none";
+        },
+        resetGame() {
+            window.location.reload()
         }
     }
 })
